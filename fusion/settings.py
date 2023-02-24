ea6ea6702bd25ade530b26f19caaac5af17f3637
andreportol <andreportol@gmail.com>
2023-02-24 17:23:43 -0400

    Tentando resolver o problema de csrf_token

--- fusion/settings.py
+++ fusion/settings.py
@@ -47,25 +47,27 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'corsheaders',
+    #'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    #'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MIDDLEWARE_CLASSES = 'django.contrib.csrf.middleware.CsrfMiddleware'
 
 CSRF_TRUSTED_ORIGINS = [
     'https://www.andre.up.railway.app/',
     'https://andre.up.railway.app/',
-    'https://andre.up.railway.app/admin/login/?next=/admin/',
+    'http://www.andre.up.railway.app/',
+    'http://andre.up.railway.app/',
 ]
 
 ROOT_URLCONF = 'fusion.urls'
